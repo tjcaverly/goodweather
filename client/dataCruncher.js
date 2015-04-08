24146205bfb48dc24ef1517181a1a22ceba9e717
Trevor Caverly <trevorcaverly@gmail.com>
2015-04-07 17:03:55 -0700

    Data cruncher ignores weights of 0 as if the user didn't care about them

--- client/dataCruncher.js
+++ client/dataCruncher.js
@@ -2,7 +2,7 @@ var goodnessAsProb = function(prefs, data, margins) {
   var total = 0;
   var maximum = 0;
   for (var key in prefs) {
-    if (key in data) {
+    if ( (key in data) && (prefs[key].weight !== 0) ) {
       var weight = prefs[key].weight;
       maximum += weight;
       total += weight * Math.min(1, Math.abs( (data[key] - prefs[key].ideal) ) / (10*margins[key]));
